9e9a4b0f8e501af757e641ccbf7068aeb299c18b
Naveen <naveentruly86@gmail.com>
2021-04-28 09:08:27 +0530

    Added:Enhancement

--- src/com/invoicegenerator/TestCabInvoiceGenerator.java
+++ src/com/invoicegenerator/TestCabInvoiceGenerator.java
@@ -34,4 +34,27 @@ public class TestCabInvoiceGenerator {
         System.out.println("Total Fare = "+totalFare);
         Assertions.assertEquals(30, totalFare,0.0);
     }
+
+
+    @Test
+    public void givenMultipleRides_shouldReturnSizeAndAverageFare(){
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+        Ride[] rides = { new Ride(20.0,4),
+                new Ride(45.0,1),
+                new Ride(75.0,1),
+                new Ride(45.5,1)};
+
+        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+        int numberOfRides = cabInvoiceGenerator.getNumberOfRides(rides);
+        double averageTotalFare = cabInvoiceGenerator.calculateAverageRideCost(rides);
+
+        System.out.println("Total Fare = " +totalFare);
+        System.out.println("Number of ride = " +numberOfRides);
+        System.out.println("Average Total Fare " +averageTotalFare);
+
+        Assertions.assertEquals(1862,totalFare,0.0);
+        Assertions.assertEquals(4,numberOfRides);
+        Assertions.assertEquals(465,averageTotalFare,0.5);
+    }
 }
